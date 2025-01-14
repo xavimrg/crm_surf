7864b48ad09db883269cd6466979ca5a727c443f
xavimrg <javier.martin.mr@gmail.com>
2025-01-14 16:17:32 +0100

    functional customer search no styles

--- client/src/app/shared/search-component/search.component.ts
+++ client/src/app/shared/search-component/search.component.ts
@@ -6,6 +6,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ProductsService } from '../services/products.service';
 import { Product } from '../interfaces/product';
 import { TranslateModule } from '@ngx-translate/core';
+import { CustomerService } from '../services/customer.service';
+import { Customer } from '../interfaces/customer';
 
 @Component({
   selector: 'app-search',
@@ -15,19 +17,49 @@ import { TranslateModule } from '@ngx-translate/core';
 })
 export class SearchComponent {
   private productService = inject(ProductsService);
+  private customerService = inject(CustomerService)
   private router = inject(Router);
 
   localProducts = signal<Product[]>([]); 
+  localCustomer = signal<Customer[]>([])
   searchedProduct = signal(''); 
+  searchedCustomer = signal('');
 
   constructor() {
     this.loadProducts();
+    this.loadCustomers();
   }
 
   updateSearch(event: Event) {
   const target = event.target as HTMLInputElement;
   this.searchedProduct.set(target.value);
+  this.searchedCustomer.set(target.value);
 }
+
+loadCustomers(){
+  this.customerService.getAllCustomers().subscribe({
+    next: (customers) => {
+      console.log('Clientes cargados:', customers);
+      this.localCustomer.set(customers);
+    },
+    error: (err) => console.error('Error loading customers:', err),
+  });}
+
+  filteredCustomers = computed(() => { 
+    const searchCustomerLower = this.searchedCustomer().toLowerCase().trim();
+    const filtered = this.localCustomer().filter((customer: Customer) => 
+    customer.customerName.toLowerCase().includes(searchCustomerLower) || 
+    customer.customerSurname.toLowerCase().includes(searchCustomerLower) ||
+    customer.companyName?.toLowerCase().includes(searchCustomerLower) ||
+    customer.companyId?.toLowerCase().includes(searchCustomerLower) || 
+    customer.email.toLowerCase().includes(searchCustomerLower));
+    return filtered;})
+
+    goToCustomer(id:number){
+      this.router.navigate([`/customer/${id}`]);
+    }
+  ;
+  
 
 
   loadProducts() {
@@ -48,7 +80,9 @@ export class SearchComponent {
     console.log('Productos filtrados:', filtered);
     return filtered;
   });
+
   goToProduct(id: number) {
     this.router.navigate([`/product/${id}`]);
   }
+
 }
